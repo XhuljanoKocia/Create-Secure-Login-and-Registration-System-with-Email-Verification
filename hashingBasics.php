c0f8f9ec109d86a9cf7bd620870168e967b6b0c3
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-02-28 14:32:26 +0100

    Understanding  and Creating our Hashing Algorithm

--- hashingBasics.php
+++ hashingBasics.php
@@ -8,11 +8,18 @@
 </head>
 <body>
   <?php
-    $password = "Trunks";
+    //Blowfish algorithm will work only if the string is 22 or more characters long else it will output *0
+    $Password = "Trunks";
     $BlowFish_Hash_Format = "$2y$10$";
     $Salt = "MYNAMEisxxhuljanokocia";
 
     echo "Length: ". strlen($Salt);
+
+    $Formatting_Blowfish_With_Salt = $BlowFish_Hash_Format . $Salt;
+    $Hash = crypt($Password, $Formatting_Blowfish_With_Salt);
+
+    echo "<br>";
+    echo $Hash;
   ?>
 </body>
 </html>
